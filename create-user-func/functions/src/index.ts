970397ed51ea6d08e79723d1a51289e3647c869a
travis544 <traviszheng544@gmail.com>
2023-12-07 16:01:55 -0800

    Firebase function for creating user document in firestore when they sign in.

---
+++ create-user-func/functions/src/index.ts
@@ -0,0 +1,40 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import * as logger from "firebase-functions/logger";
+
+// The Firebase Admin SDK to access Firestore.
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import functions = require("firebase-functions");
+import { UserRecord } from "firebase-functions/v1/auth";
+
+initializeApp();
+const firestore = getFirestore()
+console.log('I am a log entry!');
+logger.info("TEST LOG")
+async function writeUserDocument(user: UserRecord) {
+    let userDoc = firestore.collection("User").doc(user.uid)
+    return await userDoc.set({
+        email: user.email,
+        photoUrl: user.photoURL,
+        name: user.displayName
+    }, { merge: true })
+}
+
+exports.createUserDocument = functions.auth.user().onCreate((user: UserRecord) => {
+    logger.info(`Creating document for user...${user}`)
+    console.log(user)
+    return writeUserDocument(user).then(() => {
+        logger.info(`User document successfully created...${user}`)
+    }).catch(() => {
+        logger.info(`User document creation failed...${user}`)
+    })
+});
+
--- frontend/src/Authentication/AuthComponent.tsx
+++ frontend/src/Authentication/AuthComponent.tsx
@@ -3,14 +3,23 @@ import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 import { useEffect } from 'react';
 import firebase from 'firebase/compat/app';
-function AuthComponent(props: any) {
-    useEffect(() => {
-        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(props.auth);
 
+function AuthComponent(props: any) {
+    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(props.auth);
+    useEffect(() => {
         ui.start('#firebaseui-auth-container', {
             callbacks: {
-
+                signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                    // User successfully signed in.
+                    // Return type determines whether we continue the redirect automatically
+                    // or whether we leave that to developer to handle.
+                    console.log(authResult)
+                    console.log(redirectUrl)
+                    return false
+                },
             },
+            signInSuccessUrl: "http://localhost:3000/",
+            signInFlow: 'popup',
             signInOptions: [ // This array contains all the ways an user can authenticate in your application. For this example, is only by email.
                 {
                     provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -21,12 +30,15 @@ function AuthComponent(props: any) {
                 }
             ],
         });
-    }, []);
+    }, [props.auth]);
+
+
 
 
     return (
         <div>
             <div id="firebaseui-auth-container">
+
             </div>
         </div>
     )
